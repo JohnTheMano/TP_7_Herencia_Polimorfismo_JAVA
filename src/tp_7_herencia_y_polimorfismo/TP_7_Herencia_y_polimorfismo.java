06edf242fa8040813ef9bc6b82286aecc9e0e0f4
pablode <pablo.delapuente@gmail.com>
2025-10-24 11:21:12 -0300

    Primer commit TP7: Herencia y Polimorfismo

---
+++ src/tp_7_herencia_y_polimorfismo/TP_7_Herencia_y_polimorfismo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp_7_herencia_y_polimorfismo;
+
+/**
+ *
+ * @author Win
+ */
+public class TP_7_Herencia_y_polimorfismo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
